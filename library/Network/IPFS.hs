5fe9a34eb6d6b4a77509b6882244ede7a73fc6be
Daniel Holmgren <dtholmgren@gmail.com>
2019-11-26 16:34:22 -0600

    change run to runRemote

--- library/Network/IPFS.hs
+++ library/Network/IPFS.hs
@@ -4,7 +4,7 @@ module Network.IPFS
   , ipfsRun
   , getTimeout
   , MonadRemoteIPFS
-  , run
+  , runRemote
   , ipfsAdd
   , ipfsCat
   , ipfsPin
--- library/Network/IPFS/Remote/Class.hs
+++ library/Network/IPFS/Remote/Class.hs
@@ -2,7 +2,7 @@
 
 module Network.IPFS.Remote.Class
   ( MonadRemoteIPFS
-  , run
+  , runRemote
   , ipfsAdd
   , ipfsCat
   , ipfsPin
@@ -21,13 +21,13 @@ import qualified Network.IPFS.Client.Pin as Pin
 import qualified Network.IPFS.File.Types      as File
 
 class Monad m => MonadRemoteIPFS m where
-  run       :: ClientM a -> m(Either ClientError a)
+  runRemote :: ClientM a -> m(Either ClientError a)
   ipfsAdd   :: Lazy.ByteString -> m (Either ClientError CID)
   ipfsCat   :: Text            -> m (Either ClientError File.Serialized)
   ipfsPin   :: Text            -> m (Either ClientError Pin.Response)
   ipfsUnpin :: Text -> Bool    -> m (Either ClientError Pin.Response)
   -- defaults
-  ipfsAdd raw             = run <| IPFS.Client.add raw
-  ipfsCat cid             = run <| IPFS.Client.cat cid
-  ipfsPin cid             = run <| IPFS.Client.pin cid
-  ipfsUnpin cid recursive = run <| IPFS.Client.unpin cid recursive
+  ipfsAdd raw             = runRemote <| IPFS.Client.add raw
+  ipfsCat cid             = runRemote <| IPFS.Client.cat cid
+  ipfsPin cid             = runRemote <| IPFS.Client.pin cid
+  ipfsUnpin cid recursive = runRemote <| IPFS.Client.unpin cid recursive
