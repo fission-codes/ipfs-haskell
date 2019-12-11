af52b6e841aaf4d4e4ef8e3c4d06bc06536c6ab8
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-12-10 22:36:44 -0800

    Generalize logging to monad-logger

--- library/Network/IPFS/Prelude.hs
+++ library/Network/IPFS/Prelude.hs
@@ -1,6 +1,7 @@
 -- | A custom @Prelude@-like module for this project
 module Network.IPFS.Prelude
   ( module Control.Lens
+  , module Control.Monad.Logger
   , module Data.Aeson
   , module Data.Has
   , module Network.IPFS.Internal.Constraint
@@ -8,16 +9,57 @@ module Network.IPFS.Prelude
   , module RIO
   , module RIO.Process
   , identity
+  , logInfo
+  , logDebug
+  , logWarn
+  , logError
+  , logOther
   ) where
 
-import Control.Lens                ((%~), (.~), (?~), (^?))
+import Control.Lens         ((%~), (.~), (?~), (^?))
+import Control.Monad.Logger (MonadLogger (..), ToLogStr (..), LogLevel (..), logWithoutLoc)
 import Data.Aeson
 import Data.Has
+
 import Network.IPFS.Internal.Constraint
+import Network.IPFS.Internal.Orphanage.Utf8Builder ()
 
 import Flow
-import RIO                         hiding (Handler, id, timeout, ($), (&))
+
 import RIO.Process
+import RIO hiding ( Handler
+                  , LogLevel (..)
+                  , LogSource
+                  , logDebug
+                  , logDebugS
+                  , logError
+                  , logErrorS
+                  , logInfo
+                  , logInfoS
+                  , logOther
+                  , logOtherS
+                  , logWarn
+                  , logWarnS
+                  , id
+                  , timeout
+                  , ($)
+                  , (&)
+                  )
 
 identity :: a -> a
 identity a = a
+
+logInfo :: (ToLogStr msg, MonadLogger m) => msg -> m ()
+logInfo = logWithoutLoc "" LevelInfo
+
+logDebug :: (ToLogStr msg, MonadLogger m) => msg -> m ()
+logDebug = logWithoutLoc "" LevelDebug
+
+logWarn :: (ToLogStr msg, MonadLogger m) => msg -> m ()
+logWarn = logWithoutLoc "" LevelWarn
+
+logError :: (ToLogStr msg, MonadLogger m) => msg -> m ()
+logError = logWithoutLoc "" LevelError
+
+logOther :: (ToLogStr msg, MonadLogger m) => LogLevel -> msg -> m ()
+logOther = logWithoutLoc ""
