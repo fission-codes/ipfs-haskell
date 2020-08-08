de9099a80f812fe0656d7afecc3d7a8e902ea1f2
Brooklyn Zelenka <hello@brooklynzelenka.com>
2020-08-08 01:23:17 +0000

    Better nix shell

--- shell.nix
+++ shell.nix
@@ -1,32 +1,16 @@
-# shell.nix
-{ pkgs ? import <nixpkgs> {} }:
-
 let
-  hsPkgs = import ./default.nix { inherit pkgs; };
+  sources  = import ./nix/sources.nix;
+  pkgs     = import sources.nixpkgs  {};
+  unstable = import sources.unstable {};
 in
-  hsPkgs.shellFor {
-    # Include only the *local* packages of your project.
-    packages = ps: with ps; [
-      ipfs
-    ];
 
-    # Builds a Hoogle documentation index of all dependencies,
-    # and provides a "hoogle" command to search the index.
-    withHoogle = true;
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.gnumake
 
-    # You might want some extra tools in the shell (optional).
-
-    # Some common tools can be added with the `tools` argument
-    tools = { cabal = "3.2.0.0"; hlint = "2.2.11"; stack = "2.1.3.1"};
-    # See overlays/tools.nix for more details
-
-    # Some you may need to get some other way.
-    buildInputs = with pkgs.haskellPackages;
-      [
-        ghcid stm
-      ];
-
-    # Prevents cabal from choosing alternate plans, so that
-    # *all* dependencies are provided by Nix.
-    exactDeps = true;
-  }
+    # Haskell
+    unstable.ghcid
+    unstable.stack
+    unstable.stylish-haskell
+  ];
+}
