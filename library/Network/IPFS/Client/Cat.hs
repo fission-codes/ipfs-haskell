284e82158a9235d392d2ae2ebcadb934d5853c4f
Brooklyn Zelenka <be.zelenka@gmail.com>
2020-09-15 18:34:17 -0700

    File.Serialized no longer requires UTF8 for PlainText (#21)

--- library/Network/IPFS/Client/Cat.hs
+++ library/Network/IPFS/Client/Cat.hs
@@ -1,11 +1,12 @@
 module Network.IPFS.Client.Cat (API) where
 
-import Servant
+import Servant.API
 
-import qualified Network.IPFS.Client.Param as Param
-import qualified Network.IPFS.File.Types   as File
+import qualified Network.IPFS.Client.Param            as Param
+import qualified Network.IPFS.File.Types              as File
+import           Network.IPFS.MIME.RawPlainText.Types
 
 -- IPFS v0.5 disallows GET requests
 -- https://docs.ipfs.io/recent-releases/go-ipfs-0-5/#breaking-changes-upgrade-notes
 type API = Param.CID
-        :> Post '[PlainText] File.Serialized
+        :> Post '[PlainText, RawPlainText] File.Serialized
