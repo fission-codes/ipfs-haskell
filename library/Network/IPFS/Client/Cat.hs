ab66ca259766ebc3e34d04cc53630e1cd8759343
Brooklyn Zelenka <hello@brooklynzelenka.com>
2020-09-16 01:56:48 +0000

    Remove PlainText comletely

--- library/Network/IPFS/Client/Cat.hs
+++ library/Network/IPFS/Client/Cat.hs
@@ -9,4 +9,4 @@ import           Network.IPFS.MIME.RawPlainText.Types
 -- IPFS v0.5 disallows GET requests
 -- https://docs.ipfs.io/recent-releases/go-ipfs-0-5/#breaking-changes-upgrade-notes
 type API = Param.CID
-        :> Post '[PlainText, RawPlainText] File.Serialized
+        :> Post '[RawPlainText] File.Serialized
