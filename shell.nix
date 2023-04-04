c237a88ca86292704f5757b426befaeb2141b52d
Brooklyn Zelenka <hello@brooklynzelenka.com>
2023-04-03 23:34:54 -0700

    Cleanup

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 { rosetta ? false }:
   let
     sources  = import ./nix/sources.nix;
-    # commands = import ./nix/commands.nix;
 
     overrides = if rosetta then { system = "x86_64-darwin"; } else {};
 
@@ -10,26 +9,13 @@
     unstable = import sources.unstable overrides;
 
     pkgs  = if darwin.stdenv.isDarwin then darwin else nixos;
-   #  tasks = commands {
-   #    inherit pkgs;
-   #    inherit unstable;
-   #  };
 
     ghc = unstable.ghc;
 
     deps = {
-      common = [
-      #  unstable.niv
-      ];
-
-      data = [
- #       pkgs.ipfs
-   #     pkgs.zlib.dev
-      ];
-
       haskell = [
-       # unstable.stack
-        # unstable.stylish-haskell
+       unstable.stack
+       unstable.stylish-haskell
       ];
     };
   in
@@ -38,10 +24,7 @@
     inherit ghc;
     name = "Fisson";
     nativeBuildInputs = builtins.concatLists [
-   #   deps.common
-   #   deps.data
-   #   deps.haskell
-   #   tasks
+      deps.haskell
     ];
 
     shellHook = ''
